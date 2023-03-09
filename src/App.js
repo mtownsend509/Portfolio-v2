0bbe6540ad358f78ae27d105f4139d7aafe369c6
mtownsend509 <mgtownsend509@gmail.com>
2023-03-08 16:15:52 -0800

    finished the portfolio page, updated the styles on the about me page, changed to footer to be icons that redirect the user and completed the contact form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import Aboutme from './components/aboutme';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
+import Navbar from './components/navbar';
+import React, { useState } from 'react';
+import html from './Assets/images/html and css.png';
+import { SiLinkedin, SiGithub, SiTwitter } from "react-icons/si"
 
 function App() {
+
+  const [currentPage, setPage] = useState('aboutme');
+  const [display, setDisplay] = useState('none');
+
+  const renderBody = () => {
+    if(currentPage === 'aboutme') {
+      return <Aboutme />;
+    } else if (currentPage === 'portfolio') {
+      return <Portfolio 
+      display={display} setDisplay={setDisplay} html={html}
+      />;
+    } else if (currentPage === 'contact') {
+      return <Contact />;
+    }
+  }
+
+  const pageChange = (page) => setPage(page);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="headerFooter">
+      <div>
+        
+        <header>
+    
+          <h1>Matthew Townsend</h1>
+    
+          <Navbar currentPage={currentPage} pageChange={pageChange} />
+    
+        </header>
+
+        {renderBody()}
+
+        <footer>        
+          <ul className="footerList">
+            <li><a href="https://twitter.com/MattT01960695"><SiTwitter /></a></li>
+            <li><a href="https://www.linkedin.com/in/matthew-townsend-a81737223/"><SiLinkedin /></a></li>
+            <li><a href="https://github.com/mtownsend509"><SiGithub /></a></li>
+          </ul>
+        
+        </footer> 
+
+      </div>
     </div>
   );
 }
